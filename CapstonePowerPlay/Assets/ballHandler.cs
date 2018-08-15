7291a977eb62c3ada53e6ff2489324840ccc9c22
Dustin33 <38299144+Dustin33@users.noreply.github.com>
2018-08-15 14:47:17 -0400

    added networked ball
    added networked ball

---
+++ CapstonePowerPlay/Assets/ballHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ballHandler : NetworkBehaviour {
+
+
+    [SerializeField]
+    private GameObject ball;
+    [SerializeField]
+    private Transform ballSpawn;
+    
+    // Use this for initialization
+    void Awake ()
+    {
+        
+        //ballSpawn = GameObject.FindGameObjectWithTag("ballSpawn").GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            
+            CmdSpawnBall();
+            
+        }
+	}
+    [Command]
+    public void CmdSpawnBall()
+    {
+        Debug.Log("spawning ball");
+        GameObject ball1 = Instantiate(ball, ballSpawn.position, ballSpawn.rotation);
+
+        NetworkServer.Spawn(ball1);
+    }
+}
