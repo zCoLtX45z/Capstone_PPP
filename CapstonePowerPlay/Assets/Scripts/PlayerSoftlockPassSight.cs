dbb1c98351976c6cdb6e45f978a4b7d305b03b8f
Andre Giroux <andre_e_giroux@hotmial.com>
2018-09-12 15:35:31 -0400

    Added UniversalGoalScript and score keeping canvas script

--- CapstonePowerPlay/Assets/Scripts/PlayerSoftlockPassSight.cs
+++ CapstonePowerPlay/Assets/Scripts/PlayerSoftlockPassSight.cs
@@ -5,16 +5,10 @@ using UnityEngine;
 public class PlayerSoftlockPassSight : MonoBehaviour {
 
     [SerializeField]
-    private float softLockAngle = 90f;
-    //[SerializeField]
-    //private bool acceptableTargetInSight;
-    //[SerializeField]
-    //private GameObject acceptableTargetGameObject;
+    private float softLockAngle = 40f;
+
     [SerializeField]
     private float distanceToTarget;
-
-    //[SerializeField]
-    //private SphereCollider col;
 
     [SerializeField]
     private List<GameObject> listOfTeamates = new List<GameObject>();
@@ -27,22 +21,30 @@ public class PlayerSoftlockPassSight : MonoBehaviour {
     [SerializeField]
     public GameObject target;
     
-    //private GameObject currentTarget;
 
         [SerializeField]
     private List<GameObject> currentAcceptedTargets = new List<GameObject>();
 
+    private float currentClossestAngle;
+
+    private float currentAngle;
+
+    // team gameobject tag
+    private string teamTag;
+
 
     // Use this for initialization
     void Awake () {
-        foreach (GameObject playerObj in GameObject.FindGameObjectsWithTag("Player"))
+        teamTag = transform.root.tag;
+        softLockAngle = 20f;
+        foreach (GameObject playerObj in GameObject.FindGameObjectsWithTag(teamTag))
         {
             listOfTeamates.Add(playerObj);
         }
 
         for (int i = listOfTeamates.Count - 1; i >= 0; --i)
         {
-            if(listOfTeamates[i].gameObject == transform.parent.gameObject)
+            if(listOfTeamates[i].gameObject == transform.root.gameObject)
             {
                 listOfTeamates.RemoveAt(i);
                 break;
@@ -56,14 +58,7 @@ public class PlayerSoftlockPassSight : MonoBehaviour {
         for (int i = 0; i < listOfTeamates.Count; i++)
         {
             directionFromPlayer = listOfTeamates[i].transform.position - transform.position;
-
-            Debug.Log("detected somelaop");
-            directionFromPlayer = listOfTeamates[i].transform.position - transform.position;
             angle = Vector3.Angle(directionFromPlayer, transform.forward);
-
-
-            //Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
-            //Debug.DrawRay(transform.position, forward, Color.green);
 
             RaycastHit tempHit = new RaycastHit();
 
@@ -71,14 +66,11 @@ public class PlayerSoftlockPassSight : MonoBehaviour {
             {
                 if (tempHit.transform.gameObject == listOfTeamates[i])
                 {
-                    if (angle < softLockAngle / 2)
+                    if (angle < softLockAngle)
                     {
                         int temp = 0;
                         Debug.Log("TEMP: " + temp);
                         Debug.DrawRay(new Vector3(transform.position.x, transform.position.y, transform.position.z), directionFromPlayer, Color.blue);
-
-
-
 
                         if (currentAcceptedTargets.Count > 0)
                         {
@@ -132,72 +124,28 @@ public class PlayerSoftlockPassSight : MonoBehaviour {
                     }
                     Debug.DrawRay(transform.position, directionFromPlayer, Color.red);
                 }
-            }
-            
-            /*
-            else
-            {
-                for (int j = 0; j < currentAcceptedTargets.Count; j++)
-                {
-                    if (currentAcceptedTargets[j] == listOfTeamates[i])
-                    {
-                        Debug.Log("test: " + currentAcceptedTargets[j]);
-                        currentAcceptedTargets.Remove(currentAcceptedTargets[j]);
-                    }
-                }
-                Debug.DrawRay(transform.position, directionFromPlayer, Color.red);
-            }
-            */
-                
+            }                
         }
-
-
-
-        float currentClossestAngle = 360; ;
-
-        float currentAngle;
-
-
-
-
-
-
-       // currentClossestAngle = 360;
+        currentClossestAngle = 360;
         target = null;
 
         for (int i = 0; i < currentAcceptedTargets.Count; i++)
         {
             Vector3 currentObjectDirection;
             currentObjectDirection = currentAcceptedTargets[i].transform.position - transform.position;
-            currentAngle = Vector3.Angle(currentObjectDirection, transform.forward);
-
-            
-           
-                
+            currentAngle = Vector3.Angle(currentObjectDirection, transform.forward); 
                 if (currentAngle <= currentClossestAngle)
                 {
                     currentClossestAngle = currentAngle;
                     target = currentAcceptedTargets[i].gameObject;
                 }
-            
-            
-           
-
-            
-
         }
         if (target != null)
         {
             Vector3 targetObjectDirection = target.transform.position - transform.position;
             Debug.DrawRay(new Vector3(transform.position.x, transform.position.y, transform.position.z), targetObjectDirection, Color.yellow);
         }
-
-
-
     }
-
-
-    
 }
 
 
