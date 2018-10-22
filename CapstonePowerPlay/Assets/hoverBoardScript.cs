a1f42c594dae9016c4fae47abd5bc6d92d93b3a9
Dustin33 <38299144+Dustin33@users.noreply.github.com>
2018-10-22 11:03:51 -0400

    update

--- CapstonePowerPlay/Assets/hoverBoardScript.cs
+++ CapstonePowerPlay/Assets/hoverBoardScript.cs
@@ -14,10 +14,18 @@ public class hoverBoardScript : MonoBehaviour
     public float m_turnStrength = 10f;
     public float m_currTurn = 0.0f;
 
-    public int m_layerMask;
+    public LayerMask m_layerMask;
     public float m_hoverForce = 9.0f;
     public float m_hoverHeight = 2.0f;
     public GameObject[] m_hoverPoints;
+
+    // PID Controller
+    [SerializeField]
+    private float Kp = 1.1f;
+    [SerializeField]
+    private float Ki;
+    [SerializeField]
+    private float Kd;
 
     // Maximum
     [SerializeField]
@@ -27,8 +35,8 @@ public class hoverBoardScript : MonoBehaviour
 	void Start ()
     {
         m_body = GetComponent<Rigidbody>();
-        m_layerMask = 1 << LayerMask.NameToLayer("Characters");
-        m_layerMask = ~m_layerMask;
+        //m_layerMask = 1 << LayerMask.NameToLayer("Characters");
+        //m_layerMask = ~m_layerMask;
     }
 	
 	// Update is called once per frame
@@ -64,28 +72,41 @@ public class hoverBoardScript : MonoBehaviour
         for(int i = 0; i < m_hoverPoints.Length; i++)
         {
             var hoverPoint = m_hoverPoints[i];
-            if(Physics.Raycast(hoverPoint.transform.position, -Vector3.up, out hit, m_hoverHeight, m_layerMask))
+            if(Physics.Raycast(hoverPoint.transform.position, -transform.up, out hit, m_hoverHeight + 0.3f, m_layerMask))
             {
+                Debug.DrawRay(hoverPoint.transform.position, -hoverPoint.transform.up * hit.distance, Color.red);
+                float Porportional = Kp * m_hoverForce * (1.0f - (hit.distance / m_hoverHeight));
+                float Integral = Ki * m_hoverForce * (1.0f * hit.distance - (Mathf.Pow(hit.distance, 2) / (m_hoverHeight * 2))) / hit.distance;
+                float Derivative = Kp * m_hoverForce / m_hoverHeight;
                 m_body.AddForceAtPosition(Vector3.up * m_hoverForce * (1.0f - (hit.distance / m_hoverHeight)), hoverPoint.transform.position);
+                //m_body.AddForceAtPosition(Vector3.up * (Porportional + Integral + Derivative), hoverPoint.transform.position);
+                Debug.Log(hoverPoint.name + ", Force: " + (Vector3.up * m_hoverForce * (1.0f - (hit.distance / m_hoverHeight))).y + ", Distance: " + hit.distance);
             }
             else
             {
-                if (transform.position.y > hoverPoint.transform.position.y)
+                if (!Input.GetKey(KeyCode.Space))
                 {
-                    m_body.AddForceAtPosition(hoverPoint.transform.up * m_hoverForce, hoverPoint.transform.position);
-                }
-                else
-                {
-                    m_body.AddForceAtPosition(hoverPoint.transform.up * -m_hoverForce, hoverPoint.transform.position);
+                    if (transform.position.y > hoverPoint.transform.position.y)
+                    {
+                        m_body.AddForceAtPosition(hoverPoint.transform.up * m_hoverForce, hoverPoint.transform.position);
+                        Debug.DrawRay(hoverPoint.transform.position, -hoverPoint.transform.up * hit.distance, Color.black);
+                    }
+                    else
+                    {
+                        m_body.AddForceAtPosition(hoverPoint.transform.up * -m_hoverForce, hoverPoint.transform.position);
+                        Debug.DrawRay(hoverPoint.transform.position, -hoverPoint.transform.up * hit.distance, Color.blue);
+                    }
                 }
             }
         }
         //forward
-        if(Mathf.Abs(m_currThrust) > 0)
+        if (Mathf.Abs(m_currThrust) > 0)
         {
-            m_body.AddForce(transform.forward * m_currThrust, ForceMode.Acceleration);
+            m_body.AddForce(transform.forward * m_currThrust * Time.fixedDeltaTime, ForceMode.Acceleration);
             if (m_body.velocity.z > MaxSpeed)
-                m_body.velocity = new Vector3(m_body.velocity.z, m_body.velocity.y, MaxSpeed);
+                m_body.velocity = new Vector3(m_body.velocity.x, m_body.velocity.y, MaxSpeed);
+            else if (m_body.velocity.z < -MaxSpeed)
+                m_body.velocity = new Vector3(m_body.velocity.x, m_body.velocity.y, -MaxSpeed);
         }
         //turn
         if(m_currTurn > 0)
