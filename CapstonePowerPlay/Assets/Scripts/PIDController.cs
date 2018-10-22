6221e26ed540a6cbabc1923eead8cd90ce8fc206
Kevin James deCoverley <kevindecoverley@gmail.com>
2018-10-22 16:10:53 -0400

    PID Controller
    PID Controller script and a new Hoverboard Prefab

---
+++ CapstonePowerPlay/Assets/Scripts/PIDController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PIDController : MonoBehaviour {
+
+    // PID Gains
+    [SerializeField]
+    private float Kp = 1.0f;
+    [SerializeField]
+    private float Ki = 0.0f;
+    [SerializeField]
+    private float Kd = 0.0f;
+
+    // Errors
+    private float cumError = 0, prevError = 0, output;
+
+    // Clamp Seetings
+    public bool isClamp = false;
+    [SerializeField]
+    private float min = 0.0f;
+    [SerializeField]
+    private float max = 1.0f;
+
+    public void setGains(float kp, float ki, float kd)
+    {
+        Kp = kp;
+        Ki = ki;
+        Kd = kd;
+    }
+
+    public void step(float targetValue, float currentValue)
+    {
+        float error = targetValue - currentValue;
+
+        cumError += error;
+        float slope = error - prevError;
+
+        output = (Kp * error) + (Ki * cumError) + (Kd * slope);
+        if (isClamp)
+        {
+            output = Mathf.Clamp(output, min, max);
+        }
+
+        prevError = error;
+    }
+
+    public float getOutput()
+    {
+        return output;
+    }
+
+    public void EnableClamp(float minValue, float maxValue)
+    {
+        isClamp = true;
+        min = minValue;
+        max = maxValue;
+    }
+}
--- CapstonePowerPlay/Assets/hoverBoardScript.cs
+++ CapstonePowerPlay/Assets/hoverBoardScript.cs
@@ -19,13 +19,16 @@ public class hoverBoardScript : MonoBehaviour
     public float m_hoverHeight = 2.0f;
     public GameObject[] m_hoverPoints;
 
+    [SerializeField]
+    private PIDController[] PIDHoverPoints;
+
     // PID Controller
     [SerializeField]
     private float Kp = 1.1f;
     [SerializeField]
-    private float Ki;
+    private float Ki = 0.0f;
     [SerializeField]
-    private float Kd;
+    private float Kd = 0.0f;
 
     // Maximum
     [SerializeField]
@@ -37,6 +40,16 @@ public class hoverBoardScript : MonoBehaviour
         m_body = GetComponent<Rigidbody>();
         //m_layerMask = 1 << LayerMask.NameToLayer("Characters");
         //m_layerMask = ~m_layerMask;
+        if (PIDHoverPoints.Length > 0)
+        {
+            float KAdjust = m_hoverForce / m_hoverHeight;
+            for (int i = 0; i < PIDHoverPoints.Length; i++)
+            {
+                PIDHoverPoints[i].setGains(Kp * KAdjust, Ki * KAdjust, Kd * KAdjust);
+                PIDHoverPoints[i].EnableClamp(0.0f, m_hoverForce);
+                PIDHoverPoints[i].transform.localPosition = new Vector3(PIDHoverPoints[i].transform.localPosition.x, 0, PIDHoverPoints[i].transform.localPosition.z);
+            }
+        }
     }
 	
 	// Update is called once per frame
@@ -65,36 +78,69 @@ public class hoverBoardScript : MonoBehaviour
 
     void FixedUpdate()
     {
-      
-       
+
+        // Non PID Controllers
         //Hover force
-        RaycastHit hit;
-        for(int i = 0; i < m_hoverPoints.Length; i++)
+        if (m_hoverPoints.Length > 0)
         {
-            var hoverPoint = m_hoverPoints[i];
-            if(Physics.Raycast(hoverPoint.transform.position, -transform.up, out hit, m_hoverHeight + 0.3f, m_layerMask))
+            RaycastHit hit;
+            for (int i = 0; i < m_hoverPoints.Length; i++)
             {
-                Debug.DrawRay(hoverPoint.transform.position, -hoverPoint.transform.up * hit.distance, Color.red);
-                float Porportional = Kp * m_hoverForce * (1.0f - (hit.distance / m_hoverHeight));
-                float Integral = Ki * m_hoverForce * (1.0f * hit.distance - (Mathf.Pow(hit.distance, 2) / (m_hoverHeight * 2))) / hit.distance;
-                float Derivative = Kp * m_hoverForce / m_hoverHeight;
-                m_body.AddForceAtPosition(Vector3.up * m_hoverForce * (1.0f - (hit.distance / m_hoverHeight)), hoverPoint.transform.position);
-                //m_body.AddForceAtPosition(Vector3.up * (Porportional + Integral + Derivative), hoverPoint.transform.position);
-                Debug.Log(hoverPoint.name + ", Force: " + (Vector3.up * m_hoverForce * (1.0f - (hit.distance / m_hoverHeight))).y + ", Distance: " + hit.distance);
-            }
-            else
-            {
-                if (!Input.GetKey(KeyCode.Space))
+                var hoverPoint = m_hoverPoints[i];
+                if (Physics.Raycast(hoverPoint.transform.position, -transform.up, out hit, m_hoverHeight + 0.3f, m_layerMask))
                 {
-                    if (transform.position.y > hoverPoint.transform.position.y)
+                    Debug.DrawRay(hoverPoint.transform.position, -hoverPoint.transform.up * hit.distance, Color.red);
+                    m_body.AddForceAtPosition(Vector3.up * m_hoverForce * (1.0f - (hit.distance / m_hoverHeight)), hoverPoint.transform.position);
+                    //Debug.Log(hoverPoint.name + ", Force: " + (Vector3.up * m_hoverForce * (1.0f - (hit.distance / m_hoverHeight))).y + ", Distance: " + hit.distance);
+                }
+                else
+                {
+                    if (!Input.GetKey(KeyCode.Space))
                     {
-                        m_body.AddForceAtPosition(hoverPoint.transform.up * m_hoverForce, hoverPoint.transform.position);
-                        Debug.DrawRay(hoverPoint.transform.position, -hoverPoint.transform.up * hit.distance, Color.black);
+                        if (transform.position.y > hoverPoint.transform.position.y)
+                        {
+                            m_body.AddForceAtPosition(hoverPoint.transform.up * m_hoverForce, hoverPoint.transform.position);
+                            Debug.DrawRay(hoverPoint.transform.position, -hoverPoint.transform.up * hit.distance, Color.black);
+                        }
+                        else
+                        {
+                            m_body.AddForceAtPosition(hoverPoint.transform.up * -m_hoverForce, hoverPoint.transform.position);
+                            Debug.DrawRay(hoverPoint.transform.position, -hoverPoint.transform.up * hit.distance, Color.blue);
+                        }
                     }
-                    else
+                }
+            }
+        }
+        
+        if (PIDHoverPoints.Length > 0)
+        {
+
+            RaycastHit hit;
+            for (int i = 0; i < PIDHoverPoints.Length; i++)
+            {
+                PIDController temp = PIDHoverPoints[i];
+                if (Physics.Raycast(temp.gameObject.transform.position, -transform.up, out hit, m_hoverHeight + 0.3f, m_layerMask))
+                {
+                    temp.step(m_hoverHeight, hit.distance);
+                    m_body.AddForceAtPosition(Vector3.up * temp.getOutput(), temp.gameObject.transform.position);
+
+                    Debug.DrawRay(temp.gameObject.transform.position, -temp.gameObject.transform.up * hit.distance, Color.red);
+                    Debug.Log(temp.gameObject.name + ", Force: " + (Vector3.up * m_hoverForce * (1.0f - (hit.distance / m_hoverHeight))).y + ", Distance: " + hit.distance);
+                }
+                else
+                {
+                    if (!Input.GetKey(KeyCode.Space))
                     {
-                        m_body.AddForceAtPosition(hoverPoint.transform.up * -m_hoverForce, hoverPoint.transform.position);
-                        Debug.DrawRay(hoverPoint.transform.position, -hoverPoint.transform.up * hit.distance, Color.blue);
+                        if (transform.position.y > temp.gameObject.transform.position.y)
+                        {
+                            m_body.AddForceAtPosition(temp.gameObject.transform.up * m_hoverForce, temp.gameObject.transform.position);
+                            Debug.DrawRay(temp.gameObject.transform.position, -temp.gameObject.transform.up * hit.distance, Color.black);
+                        }
+                        else
+                        {
+                            m_body.AddForceAtPosition(temp.gameObject.transform.up * -m_hoverForce, temp.gameObject.transform.position);
+                            Debug.DrawRay(temp.gameObject.transform.position, -temp.gameObject.transform.up * hit.distance, Color.blue);
+                        }
                     }
                 }
             }
