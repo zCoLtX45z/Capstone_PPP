c5737e946e59684c826579552285d65197d6da16
Andre Giroux <andre_e_giroux@hotmial.com>
2018-11-12 11:23:49 -0500

    u

---
+++ CapstonePowerPlay/Assets/Scripts/InfluanceRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfluanceRotation : MonoBehaviour {
+
+    //[SerializeField]
+    //private Transform referenceXZ;
+    //[SerializeField]
+    //private Transform referenceY;
+
+    [SerializeField]
+    private Transform child; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.eulerAngles = new Vector3(referenceXZ.eulerAngles.x, 0, referenceXZ.eulerAngles.z);
+        //referenceY.eulerAngles = new Vector3(0, referenceXZ.eulerAngles.y, 0);
+        child.localEulerAngles = new Vector3(child.localEulerAngles.x, -transform.root.eulerAngles.y, child.localEulerAngles.z);
+        
+
+    }
+}
