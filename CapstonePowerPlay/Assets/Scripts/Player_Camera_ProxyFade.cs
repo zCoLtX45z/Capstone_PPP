9f4d2e006744a9a419d2ef719781f743fe3dede8
Andre Giroux <andre_e_giroux@hotmial.com>
2019-01-08 17:16:42 -0500

    Material shading outline set

--- CapstonePowerPlay/Assets/Scripts/Player_Camera_ProxyFade.cs
+++ CapstonePowerPlay/Assets/Scripts/Player_Camera_ProxyFade.cs
@@ -8,25 +8,63 @@ public class Player_Camera_ProxyFade : MonoBehaviour {
     [SerializeField]
     private Renderer playerRenderer = null;
 
+    [SerializeField]
+    private Material fadingMaterial = null;
+
+    [SerializeField]
+    private Material standardMaterial = null;
+   
+
+    // can only fade mats if their renderin mode is eitehr fade or transparent
+    // change the mat to an exact copy of the mat with the only rendering mode being change back and fourth between Opaque and Fade/Transperant 
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        // change this to see the distance between player and their camera
+        ///
+
+        //if(Input.GetKeyDown(KeyCode.Q))
+        //{
+        //    Debug.Log("Q has been pressed");
+        //    // set the material to fade
+
+        //    playerRenderer.material = new Material(Shader.Find("Fade"));
+        //}
+
+        //if(Input.GetKey(KeyCode.R))
+        //{
+        //    Debug.Log("R is being pressed");
+        //    FadeOut();
+        //}
+        //if(Input.GetKey(KeyCode.T))
+        //{
+        //    Debug.Log("T is being pressed");
+        //    FadeIn();
+        //}
+
+    }
 
 
     private void FadeOut()
     {
-        while(playerRenderer.material.color.a>0)
+        if(playerRenderer.material.color.a>0)
         {
-            //playerRenderer.material.color -= playerRenderer.material Time.deltaTime / 2;
+            Debug.Log("fading");
+            playerRenderer.material.color = new Color(playerRenderer.material.color.r, playerRenderer.material.color.g, playerRenderer.material.color.b, playerRenderer.material.color.a - Time.deltaTime / 2);
         }
+        Debug.Log("playerRenderer.material.color.a : " + playerRenderer.material.color.a);
+    }
+
+    private void FadeIn()
+    {
+        if (playerRenderer.material.color.a < 1)
+        {
+            Debug.Log("fading");
+            playerRenderer.material.color = new Color(playerRenderer.material.color.r, playerRenderer.material.color.g, playerRenderer.material.color.b, playerRenderer.material.color.a + Time.deltaTime / 2);
+        }
+        Debug.Log("playerRenderer.material.color.a : " + playerRenderer.material.color.a);
     }
 
 }
