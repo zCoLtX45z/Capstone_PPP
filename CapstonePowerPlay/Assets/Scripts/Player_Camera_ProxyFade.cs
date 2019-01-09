0a6859c5dd16a4abe915f82714b045147c49beb8
Andre Giroux <andre_e_giroux@hotmial.com>
2019-01-09 16:30:43 -0500

    Fade works

--- CapstonePowerPlay/Assets/Scripts/Player_Camera_ProxyFade.cs
+++ CapstonePowerPlay/Assets/Scripts/Player_Camera_ProxyFade.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class Player_Camera_ProxyFade : MonoBehaviour {
 
 
+
+    [SerializeField]
+    private Transform playerTransform = null;
+
+    [SerializeField]
+    private Transform cameraTransform = null;
+
+
+    [SerializeField]
+    private float maxDistance = 1.5f;
+
+
     [SerializeField]
     private Renderer playerRenderer = null;
 
@@ -13,6 +25,9 @@ public class Player_Camera_ProxyFade : MonoBehaviour {
 
     [SerializeField]
     private Material standardMaterial = null;
+
+    [SerializeField]
+    private float distanceFadeOffSet = 0.0f;
    
 
     // can only fade mats if their renderin mode is eitehr fade or transparent
@@ -21,6 +36,30 @@ public class Player_Camera_ProxyFade : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+
+
+
+        //Debug.Log("distance between camera and obj: " + (cameraTransform.position - playerTransform.position).magnitude);
+
+
+        float distance = (cameraTransform.position - playerTransform.position).magnitude;
+
+        if (playerRenderer.material.color.a < 1)
+        {
+            Debug.Log("Within range");
+            playerRenderer.material = fadingMaterial;
+        }
+        else
+        {
+            Debug.Log("Out of range");
+            playerRenderer.material = standardMaterial;
+        }
+
+        
+
+        playerRenderer.material.color = new Color(playerRenderer.material.color.r, playerRenderer.material.color.g, playerRenderer.material.color.b,/**/ Mathf.Clamp((distance / maxDistance) - distanceFadeOffSet, 0, 1)/**/);
+        Debug.Log("mat a: " + playerRenderer.material.color.a);
+
 
         // change this to see the distance between player and their camera
         ///
