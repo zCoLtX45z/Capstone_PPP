5bb750f35d2642dba46ceea2ebab69b44c065ecc
Andre Giroux <andre_e_giroux@hotmial.com>
2019-01-14 14:05:14 -0500

    update_still not working

---
+++ CapstonePowerPlay/Assets/Scripts/LookAtPostionFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPostionFollow : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField]
+    private Transform lookatPoint;
+
+
+	void Start () {
+        transform.parent = null;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        transform.position = lookatPoint.position;
+
+        transform.rotation = Quaternion.Euler(lookatPoint.root.eulerAngles.x, lookatPoint.root.eulerAngles.y, lookatPoint.root.eulerAngles.z);
+
+
+    }
+}
