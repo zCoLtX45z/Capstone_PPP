06661f6412ba097d8ad14935e3c85de05c3c551e
Kevin James deCoverley <kevindecoverley@gmail.com>
2019-01-14 16:50:42 -0500

    Out of Bounnds

---
+++ CapstonePowerPlay/Assets/Scripts/OutOfBoundsCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBoundsCheck : MonoBehaviour {
+
+    [SerializeField]
+    private hoverBoardScript PlayerBoard;
+
+    //void OnCollisionStay(Collision c)
+    //{
+    //    if (c.gameObject.tag == "OutOfBounds")
+    //    {
+    //        PlayerBoard.FlipCharacter();
+    //    }
+    //}
+
+    void OnTriggerStay(Collider c)
+    {
+        if (c.gameObject.tag == "OutOfBounds")
+        {
+            PlayerBoard.FlipCharacter();
+        }
+    }
+}
