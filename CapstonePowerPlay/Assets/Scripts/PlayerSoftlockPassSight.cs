f30fb53c6fdebc50d4b2e1301aeb95c961e0bf8c
Andre Giroux <andre_e_giroux@hotmial.com>
2019-01-17 16:16:05 -0500

    f

--- CapstonePowerPlay/Assets/Scripts/PlayerSoftlockPassSight.cs
+++ CapstonePowerPlay/Assets/Scripts/PlayerSoftlockPassSight.cs
@@ -268,7 +268,7 @@ public class PlayerSoftlockPassSight : MonoBehaviour
                     // set currentClossestAngle as the  currentAngle;
                     currentClossestAngle = currentAngle;
                     // set the target as the currently checked gameObject in the currentAcceptedTargets list 
-                    target = currentAcceptedTargets[i].transform.GetChild(currentAcceptedTargets[i].transform.childCount - 1).gameObject; /* <---- IF NO LONGER WORK TARGET CHANGE   (ORIGINAL:  target = currentAcceptedTargets[i].gameObject;)  */
+                    target = currentAcceptedTargets[i].gameObject; /* <---- IF NO LONGER WORK TARGET CHANGE   (ORIGINAL:  target = currentAcceptedTargets[i].gameObject;)  */
                     // set target position to center the players
                     targetPosition = new Vector3(currentAcceptedTargets[i].transform.position.x, currentAcceptedTargets[i].transform.position.y + 1, currentAcceptedTargets[i].transform.position.z);
                 }
