c298b71de6b59295015b8fdd6d1313d630f20d04
Andre Giroux <andre_e_giroux@hotmial.com>
2019-01-17 18:58:34 -0500

    update

--- CapstonePowerPlay/Assets/Scripts/LookAtPostionFollow.cs
+++ CapstonePowerPlay/Assets/Scripts/LookAtPostionFollow.cs
@@ -23,7 +23,7 @@ public class LookAtPostionFollow : MonoBehaviour {
 
         RaycastHit hit;
         // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(board.position, board.TransformDirection(Vector3.down), out hit, Mathf.Infinity, layerMask))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, layerMask))
         {
             //Debug.Log("Hit: " + hit.transform.name);
            // Debug.DrawRay(transform.position, -transform.up, Color.magenta, Mathf.Infinity);
