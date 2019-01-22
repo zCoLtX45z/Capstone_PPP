cf2e539e699ebcee4a598626c2277feb223bd969
K-D-Kevin <38299126+K-D-Kevin@users.noreply.github.com>
2019-01-22 15:09:56 -0500

    Ball Indicator

---
+++ CapstonePowerPlay/Assets/Scripts/UI_TrackAndScale.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_TrackAndScale : MonoBehaviour {
+
+    [SerializeField]
+    private RectTransform RT;
+
+    private PlayerColor Target;
+    private PlayerColor[] Targets;
+	// Use this for initialization
+	void Start () {
+        Targets = FindObjectsOfType<PlayerColor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
