baa981f8a6d06a0a531cbb6e3e4cc4a1c984c6e6
DESKTOP-2DHBK59\marcg <marcguy469@gmail.com>
2019-01-24 19:48:35 -0500

    doors

---
+++ CapstonePowerPlay/Assets/DoorSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DoorSpawner : NetworkBehaviour {
+    [SerializeField]
+    private Transform doorSpawn;
+    [SerializeField]
+    private GameObject doorPrefab;
+    // Use this for initialization
+    void Start()
+    {
+        CmdSpawnNet();
+
+    }
+
+    [Command]
+    public void CmdSpawnNet()
+    {
+        GameObject TempNet;
+        TempNet = Instantiate(doorPrefab, doorSpawn.position, doorSpawn.rotation);
+        NetworkServer.Spawn(TempNet);
+    }
+}
