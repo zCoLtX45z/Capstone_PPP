7633a6e3f924bfe2c758af13a7212b748d9f6ed3
Dustin33 <38299144+Dustin33@users.noreply.github.com>
2019-02-11 17:01:04 -0500

    added first pass of ragdoll
    ragdoll and particle trails first attempt

---
+++ CapstonePowerPlay/Assets/Ragdoll.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody[] RBs;
+    [SerializeField]
+    private ParticleSystem PS;
+	// Use this for initialization
+	void Awake ()
+    {
+		RBs = gameObject.GetComponentsInChildren<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.P))
+        {
+            EnableRagdoll();
+        }
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            DisableRagdoll();
+        }
+	}
+
+    public void EnableRagdoll()
+    {
+        foreach(Rigidbody rb in RBs)
+        {
+            rb.isKinematic = false;
+        }
+        gameObject.GetComponentInChildren<Animator>().enabled = false;
+    }
+
+    public void DisableRagdoll()
+    {
+        foreach (Rigidbody rb in RBs)
+        {
+            rb.isKinematic = true;
+        }
+        gameObject.GetComponentInChildren<Animator>().enabled = true;
+        PS.Play();
+    }
+}
