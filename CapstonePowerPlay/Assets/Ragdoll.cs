983df382830674001a9ba7272a9f9799f7a3942b
Dustin33 <38299144+Dustin33@users.noreply.github.com>
2019-02-14 14:53:57 -0500

    new arena added

--- CapstonePowerPlay/Assets/Ragdoll.cs
+++ CapstonePowerPlay/Assets/Ragdoll.cs
@@ -1,56 +1,97 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class Ragdoll : MonoBehaviour
 {
-    [SerializeField]
-    private Rigidbody[] RBs;
+    //[SerializeField]
+    //private Rigidbody[] RBs;
     [SerializeField]
     private ParticleSystem PS;
     [SerializeField]
     private ParticleSystem PS1;
+    //[SerializeField]
+    //private Rigidbody BaseRb;
+    //[SerializeField]
+    //private Animator Anim;
 	// Use this for initialization
 	void Awake ()
     {
-		RBs = gameObject.GetComponentsInChildren<Rigidbody>();
+  //      Anim = gameObject.GetComponentInChildren<Animator>();
+  //      BaseRb = gameObject.GetComponent<Rigidbody>();
+		//RBs = gameObject.GetComponentsInChildren<Rigidbody>();
+        //foreach (Rigidbody rb in RBs)
+        //{
+        //    if(rb.gameObject.tag != "Shield")
+        //    {
+        //        rb.isKinematic = true;
+        //    }
+
+        //}
+        //foreach (Rigidbody rb in RBs)
+        //{
+        //    if (rb.gameObject.tag != "Shield" || rb.gameObject.tag != "Team 1" || rb.gameObject.tag != "Team 2")
+        //    {
+        //        rb.mass = 0.01f;
+        //    }
+
+        //}
+        //BaseRb.isKinematic = false;
+        //BaseRb.mass = 100;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if(Input.GetKeyDown(KeyCode.P))
-        {
-            EnableRagdoll();
-        }
+		//if(Input.GetKeyDown(KeyCode.P))
+  //      {
+  //          EnableRagdoll();
+  //      }
         if(Input.GetKeyDown(KeyCode.L))
         {
-            DisableRagdoll();
+            CmdGetUp();
         }
 	}
 
-    public void EnableRagdoll()
-    {
-        foreach(Rigidbody rb in RBs)
-        {
-            rb.isKinematic = false;
-        }
-        gameObject.GetComponentInChildren<Animator>().enabled = false;
-    }
+    //public void EnableRagdoll()
+    //{
 
-    public void DisableRagdoll()
+    //    //Death animation forward, copy for death anim backwards
+    //    //foreach (Rigidbody rb in RBs)
+    //    //{
+    //    //    if (rb.gameObject.tag != "Shield" || rb.gameObject.tag != "Team 1" || rb.gameObject.tag != "Team 2")
+    //    //    {
+    //    //        //rb.isKinematic = false;
+    //    //        rb.mass = 2.0f;
+    //    //    }
+    //    //}
+    //    //BaseRb.isKinematic = true;
+    //    //Anim.enabled = false;
+    //}
+    [Command]
+    public void CmdGetUp()
     {
-        //foreach (Rigidbody rb in RBs)
-        //{
-        //    rb.isKinematic = true;
-        //}
-        gameObject.GetComponentInChildren<Animator>().enabled = true;
-        PlayEffects();
-    }
-
-    public void PlayEffects()
-    {
+        Debug.Log("getting up");
         PS.Play();
         PS1.Play();
+        //foreach (Rigidbody rb in RBs)
+        //{
+        //    if (rb.gameObject.tag != "Shield" || rb.gameObject.tag != "Team 1" || rb.gameObject.tag != "Team 2")
+        //    {
+        //        //rb.isKinematic = true;
+        //        rb.mass = 0.01f;
+        //        rb.velocity = Vector3.zero;
+        //    }
+        //}
+        //BaseRb.isKinematic = false;
+        //Anim.enabled = true;
+
     }
+   
+    //public void CmdPlayGetUpEffects()
+    //{
+    //    PS.Play();
+    //    PS1.Play();
+    //}
 }
--- CapstonePowerPlay/Assets/SpeedStrip.cs
+++ CapstonePowerPlay/Assets/SpeedStrip.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedStrip : MonoBehaviour {
-
+public class SpeedStrip : MonoBehaviour
+{
+    [SerializeField]
+    private float LifeTime = 7.0f;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
