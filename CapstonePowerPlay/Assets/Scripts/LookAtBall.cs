4cb96746e9562e5eb8cdae8f1c23043240e00b80
Kevin James deCoverley <kevindecoverley@gmail.com>
2019-02-20 14:24:39 -0500

    Revert "Merge branch 'master' into Kevin"
    This reverts commit 0865488914e523f3fec8a990f1b8e76804f0c04f, reversing
    changes made to b631eadbcc3dd22f31508dd138f7787406b26867.

--- CapstonePowerPlay/Assets/Scripts/LookAtBall.cs
+++ CapstonePowerPlay/Assets/Scripts/LookAtBall.cs
@@ -8,76 +8,13 @@ public class LookAtBall : MonoBehaviour {
 
     public bool allow = false;
 
-    private bool hardLock;
-
-    [SerializeField]
-    private float maxLookAtSpeed;
-
-    private float lookAtSpeed;
-
-    [SerializeField]
-    private float accelerationBoost = 1;
-
 	// Use this for initialization
 	void Start () {
         lookatBall = GameObject.FindGameObjectWithTag("Ball").transform;
-        hardLock = false;
     }
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-        if (allow)
-        {
-           
-
-            if(lookAtSpeed < maxLookAtSpeed)
-            {
-                lookAtSpeed += Time.time * accelerationBoost;
-            }
-            else
-            {
-                lookAtSpeed = maxLookAtSpeed;
-            }
-
-            if (!hardLock)
-            {
-                Debug.Log("Alert");
-                Vector3 direction = lookatBall.position - transform.position;
-
-                //Vector3 newDir = Vector3.RotateTowards(transform.forward, direction, lookAtSpeed * Time.time, 0.0f);
-
-                //transform.rotation = Quaternion.LookRotation(newDir);
-
-
-                Quaternion rot = Quaternion.LookRotation(direction);
-                // slerp to the desired rotation over time
-                transform.rotation = Quaternion.Slerp(transform.rotation, rot, lookAtSpeed * Time.deltaTime);
-
-
-                if (Vector3.Angle(direction, transform.forward) ==0)
-                {
-                    Debug.Log("activate hard lock");
-                    hardLock = true;
-                }
-               
-
-            }
-            if (hardLock)
-            {
-                transform.LookAt(lookatBall, transform.up);
-            }
-
-        }
-        else
-        {
-            if(lookAtSpeed >= maxLookAtSpeed)
-            {
-                lookAtSpeed = 0;
-            }
-            if(hardLock)
-            {
-                hardLock = false;
-            }
-        }
-    }
+	void Update () {
+        transform.LookAt(lookatBall);
+	}
 }
