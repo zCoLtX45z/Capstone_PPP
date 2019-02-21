72aed7ad908d0d0a4d6a5cb21314801e1b7e245b
Andre Giroux <andre_e_giroux@hotmial.com>
2019-02-21 18:14:32 -0500

    Merge pull request #208 from zCoLtX45z/Kevin
    Kevin

--- CapstonePowerPlay/Assets/Scripts/Item.cs
+++ CapstonePowerPlay/Assets/Scripts/Item.cs
@@ -10,6 +10,21 @@ public class Item : MonoBehaviour {
     private MeshFilter ItemMeshFilter;
     [SerializeField]
     private Texture ItemSprite;
+    [SerializeField]
+    private Behaviour[] DisableBehaviours;
+    [SerializeField]
+    private GameObject[] DisableObjects;
+    [SerializeField]
+    private MeshRenderer[] MeshRenderers;
+    [SerializeField]
+    private BoxCollider PlacingCollider;
+    public Vector3 BoxSize;
+    public Vector3 BoxOffset;
+
+    public BoxCollider GetCollider()
+    {
+        return PlacingCollider;
+    }
 
     public Mesh GetMesh()
     {
@@ -26,5 +41,37 @@ public class Item : MonoBehaviour {
     {
         transform.position = pos;
         transform.up = normal;
+    }
+
+    public void Disable()
+    {
+        foreach (Behaviour b in DisableBehaviours)
+        {
+            b.enabled = false;
+        }
+        foreach (GameObject g in DisableObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    public void Enable()
+    {
+        foreach (Behaviour b in DisableBehaviours)
+        {
+            b.enabled = true;
+        }
+        foreach (GameObject g in DisableObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    public void ChangeMaterials(Material mat)
+    {
+        foreach (MeshRenderer MR in MeshRenderers)
+        {
+            MR.material = mat;
+        }
     }
 }
