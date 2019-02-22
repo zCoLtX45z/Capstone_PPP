e0df5f79b0b0f3628c758ad91f75673e74d4ac2f
Andre Giroux <andre_e_giroux@hotmial.com>
2019-02-22 11:28:23 -0500

    update

---
+++ CapstonePowerPlay/Assets/Scripts/PingServer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PingServer : NetworkBehaviour {
+
+    [SerializeField]
+    private float maxTime;
+
+    private float currentTime;
+
+	// Use this for initialization
+	void Start () {
+        currentTime = maxTime;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(currentTime > 0)
+        {
+            currentTime -= Time.time; 
+        }
+        else
+        {
+            currentTime = maxTime;
+
+        }
+
+
+	}
+
+    [ClientRpc]
+    private void RpcPingFunction()
+    {
+        CmdPingFunction();
+    }
+
+    [Command]
+    private void CmdPingFunction()
+    {
+
+    }
+
+
+}
