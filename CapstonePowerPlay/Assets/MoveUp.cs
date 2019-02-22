4a64b3b5077c18919b8daf020149bc44c36a142c
Dustin33 <38299144+Dustin33@users.noreply.github.com>
2019-02-22 11:40:45 -0500

    fixed coding error(deleted keepAlive Script)

---
+++ CapstonePowerPlay/Assets/MoveUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUp : MonoBehaviour {
+
+   
+    public Transform FinalSpot;
+    // Use this for initialization
+    void Start()
+    {
+        FinalSpot = FindObjectOfType<netSpawner>().GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(FinalSpot.transform.position.y);
+        if (gameObject.transform.position.y < FinalSpot.transform.position.y)
+        {
+            Debug.Log("moving on up");
+            transform.position += Vector3.up * 1 * Time.deltaTime;
+        }
+    }
+}
