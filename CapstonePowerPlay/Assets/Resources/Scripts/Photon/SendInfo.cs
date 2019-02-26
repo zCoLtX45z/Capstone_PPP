e690554c6233cd8596bbece213c18a1ce9d8eb83
K-D-Kevin <38299126+K-D-Kevin@users.noreply.github.com>
2019-02-26 14:59:10 -0500

    Import Other Project Files

---
+++ CapstonePowerPlay/Assets/Resources/Scripts/Photon/SendInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class SendInfo : MonoBehaviourPun, IPunObservable {
+
+    
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        
+    }
+}
