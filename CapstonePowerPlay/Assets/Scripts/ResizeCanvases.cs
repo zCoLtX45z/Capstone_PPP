f2d487e7336bff0799e8a39fb1bceec57c1e23c9
Andre Giroux <andre_e_giroux@hotmial.com>
2019-02-28 16:51:20 -0500

    update

---
+++ CapstonePowerPlay/Assets/Scripts/ResizeCanvases.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeCanvases : MonoBehaviour {
+
+    [SerializeField]
+    private RectTransform[] canvasTransform;
+
+
+	// Use this for initialization
+	void Start () {
+        for (int i = canvasTransform.Length - 1; i >= 0; i--)
+        {
+            canvasTransform[i].sizeDelta = new Vector2(Screen.width, Screen.height);
+        }
+        
+	}
+	
+	//// Update is called once per frame
+	//void Update () {
+ //       Debug.Log("w: " + Screen.currentResolution.width + " / h: " + Screen.currentResolution.height);
+ //   }
+}
