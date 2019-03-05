7a37f6a1550dd12592c88bfb51104151289bf971
Andre Giroux <andre_e_giroux@hotmial.com>
2019-03-05 15:33:36 -0500

    u

--- CapstonePowerPlay/Assets/Scripts/Ball.cs
+++ CapstonePowerPlay/Assets/Scripts/Ball.cs
@@ -439,8 +439,8 @@ public class Ball : MonoBehaviour
         //transform.gameObject.layer = 0;
         Debug.Log("unParetning Ball shoot. old parent: " + transform.parent);
         Debug.Log("Hand: " + Hand);
-        transform.GetComponentInParent<BallHandling>().ReturnHand().DetachChildren();
-        Hand.DetachChildren();
+        //transform.GetComponentInParent<BallHandling>().ReturnHand().DetachChildren();
+        //Hand.DetachChildren();
         transform.SetParent(null);
         Thrown = true;
         CanBeCaughtTimer = 0.15f;
