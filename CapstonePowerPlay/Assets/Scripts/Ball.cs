25605e7f060932e425e5a14d742ebdc9fd918575
Dustin33 <38299144+Dustin33@users.noreply.github.com>
2019-03-06 17:44:20 -0500

    pillars added and textured

--- CapstonePowerPlay/Assets/Scripts/Ball.cs
+++ CapstonePowerPlay/Assets/Scripts/Ball.cs
@@ -353,6 +353,7 @@ public class Ball : MonoBehaviour
             if(!PV.IsMine)
             {
                 PV.TransferOwnership(c.gameObject.GetPhotonView().ViewID);
+                Debug.Log("transfering ownership");
             }
 
             if(!hasBeenPickedUpBefore)
