c144a8ad020ee3d27b125268e9d575d619ec9ab5
DESKTOP-2DHBK59\marcg <marcguy469@gmail.com>
2019-03-07 17:42:49 -0500

    more settings

--- CapstonePowerPlay/Assets/Scripts/PlayerSoundSettings.cs
+++ CapstonePowerPlay/Assets/Scripts/PlayerSoundSettings.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerSoundSettings : MonoBehaviour {
 
 
     public float musicVol;
+    public float hoverVol;
+
+
+    public Slider musicSlider;
+    public Slider SoundFXSlider;
+
+
+    private musicMenu muse;
+    private HoverBoardSounds hov;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +24,7 @@ public class PlayerSoundSettings : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        musicVol = musicSlider.value;
+        hoverVol = SoundFXSlider.value;
 	}
 }
