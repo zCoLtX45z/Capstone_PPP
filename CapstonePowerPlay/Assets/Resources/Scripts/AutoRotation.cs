900321b07101d7e975d2454acad05f2faa077cef
Kevin James deCoverley <kevindecoverley@gmail.com>
2019-03-12 13:35:51 -0400

    Placing fix,

---
+++ CapstonePowerPlay/Assets/Resources/Scripts/AutoRotation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotation : MonoBehaviour {
+
+    // Adjustables
+    [SerializeField]
+    private float RotationSpeed = 10;
+    [SerializeField]
+    private LayerMask GroundLayers;
+
+    // Components
+    [SerializeField]
+    private Camera Cam;
+
+    // Hiddenn variables
+    [HideInInspector]
+    public bool IsActive = true;
+    private bool PlayerSetActive = true;
+	
+	// Update is called once per frame
+	void Update () {
+		
+        // Set if the player wants autorotation on
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            PlayerSetActive = !PlayerSetActive;
+        }
+
+        // If the players autorotation is active, rotatte
+        if (IsActive && PlayerSetActive)
+        {
+
+        }
+	}
+}
