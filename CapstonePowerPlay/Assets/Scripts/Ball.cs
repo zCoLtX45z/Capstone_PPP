0a98b9474aa34edcc892f9106d5f155908d38c3b
Andre Giroux <andre_e_giroux@hotmial.com>
2019-03-14 13:55:59 -0400

    Basic animation added

--- CapstonePowerPlay/Assets/Scripts/Ball.cs
+++ CapstonePowerPlay/Assets/Scripts/Ball.cs
@@ -75,6 +75,9 @@ public class Ball : MonoBehaviour
     [SerializeField]
     private bool inPlay = false;
 
+    [SerializeField]
+    private Animation animFloat = null;
+
     // Use this for initialization
     void Start()
     {
@@ -85,6 +88,8 @@ public class Ball : MonoBehaviour
         nSpawner = FindObjectOfType<netSpawner>();
         rTimerScript = FindObjectOfType<RoundManager>();
         SoftCol.radius = PickUpRadius;
+
+        animFloat = transform.GetChild(0).GetComponent<Animation>();
     }
 
     private void Update()
@@ -269,6 +274,7 @@ public class Ball : MonoBehaviour
         if (!inPlay)
         {
             inPlay = true;
+            animFloat.enabled = false;
         }
         if (Thrown)
         {
@@ -286,6 +292,7 @@ public class Ball : MonoBehaviour
         if(!inPlay)
         {
             inPlay = true;
+            animFloat.enabled = false;
         }
         //Debug.Log("PLAYER HAS ENTERED THE AREA!!!");
         gameObject.layer = 2;
@@ -452,6 +459,7 @@ public class Ball : MonoBehaviour
         if (!inPlay)
         {
             inPlay = true;
+            animFloat.enabled = false;
         }
         //Debug.Log("PLAYER HAS LEFT THE AREA!!!");
         HardCol.isTrigger = false;
