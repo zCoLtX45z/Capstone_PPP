33ee88101df7d046ce94eb3bd6347cc544a1e353
DESKTOP-2DHBK59\marcg <marcguy469@gmail.com>
2019-03-14 18:06:21 -0400

    InGameSoundSEtting

---
+++ CapstonePowerPlay/Assets/Scripts/InGameSoundSettings.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameSoundSettings : MonoBehaviour {
+
+
+    public float musicVol;
+    public float SoundFXVol;
+    public float VoiceVol;
+
+    public Slider musicSlider;
+    public Slider SoundFXSlider;
+    public Slider voiceSlider;
+
+    private PlayerSoundSettings PS;
+    private PlayerInGameMusic PGM;
+    private HoverBoardSounds HV;
+    private BallSoundManager BS;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        PS = FindObjectOfType<PlayerSoundSettings>();
+        PGM = FindObjectOfType<PlayerInGameMusic>();
+        HV = FindObjectOfType<HoverBoardSounds>();
+        BS = FindObjectOfType<BallSoundManager>();
+        musicVol = PS.musicVol;
+        SoundFXVol = PS.SoundFXVol;
+        VoiceVol = PS.VoiceVol;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (musicSlider != null)
+        {
+            musicVol = musicSlider.value;
+            PGM.MusicVol = musicVol;
+        }
+        if (SoundFXSlider != null)
+        {
+            SoundFXVol = SoundFXSlider.value;
+            HV.HovVol = SoundFXVol;
+            BS.bounceVol = SoundFXVol;
+        }
+        if (voiceSlider != null)
+        {
+            VoiceVol = voiceSlider.value;
+
+        }
+
+    }
+}
