3d7728918b379dde8a3f41753d86a1064041b3e7
DESKTOP-2DHBK59\marcg <marcguy469@gmail.com>
2019-03-14 16:55:02 -0400

    musicchange

--- CapstonePowerPlay/Assets/Scripts/musicMenu.cs
+++ CapstonePowerPlay/Assets/Scripts/musicMenu.cs
@@ -6,20 +6,40 @@ public class musicMenu : MonoBehaviour {
 
     private AudioSource Src;
 
-    private float musicVolume = 0.05f;
+    public AudioClip[] music;
+
+    private float musicVolume = 1f;
 
 	// Use this for initialization
 	void Start () {
         Src = GetComponent<AudioSource>();
-	}
+        //if (!Src.playOnAwake)
+        //{
+        //    Src.clip = music[0];
+        //    Src.Play();
+        //}
+    }
 	
 	// Update is called once per frame
 	void Update () {
         Src.volume = musicVolume;
-	}
+        //if (!Src.isPlaying)
+        //{
+        //    Src.clip = music[Random.Range(0, music.Length)];
+        //    Src.Play();
+        //}
+    }
 
     public void SetVolume(float vol)
     {
         musicVolume = vol;
     }
+
+    //void PlayNextSong()
+    //{
+    //    Debug.Log("playmusicisCalled");
+    //    Src.clip = music[Random.Range(0, music.Length)];
+    //    Src.Play();
+    //    Invoke("PlayNextSong", Src.clip.length);
+    //}
 }
